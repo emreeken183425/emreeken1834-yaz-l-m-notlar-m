b91f4d487e5cc2a66fcc1f23fff663e70e8acf2c
emreeken183425 <emreeken1834@gmail.com>
2022-06-11 04:45:14 +0300

    loading

--- javascript/class-notes/js10_dom-examples/app.js
+++ javascript/class-notes/js10_dom-examples/app.js
@@ -24,42 +24,47 @@ const buttons = ul.closest('.container').querySelectorAll('.btn');
 console.log(buttons);
 //************************************************* */
 
-ekleBtn.onclick = function() {
-    if (!dilInput.value) {
-        alert('Lütfen bir dil giriniz');
-    } else {
-        ul.innerHTML += ` <li>${dilInput.value}</li>`;
-        dilInput.value = '';
-        javascriptKontrol();
-    }
+ekleBtn.onclick = function () {
+  if (!dilInput.value) {
+    alert('Lütfen bir dil giriniz');
+  } else {
+    ul.innerHTML += ` <li>${dilInput.value}</li>`;
+    dilInput.value = '';
+    javascriptKontrol();
+  }
 };
 
 const javascriptKontrol = () => {
-    document.querySelectorAll('ul li').forEach((dil) => {
-        const kucukHarf = dil.textContent.toLowerCase();
-        if (kucukHarf === 'javascript') {
-            // dil.className = 'red';
-            //?Alternatif yöntem
-            dil.setAttribute('class', 'red');
-        }
-    });
+  document.querySelectorAll('ul li').forEach((dil) => {
+    const kucukHarf = dil.textContent.toLowerCase();
+    if (kucukHarf === 'javascript') {
+      // dil.className = 'red';
+      //?Alternatif yöntem
+      dil.setAttribute('class', 'red');
+    }
+  });
 };
 
-silBtn.onclick = function() {
-    ul.childElementCount > 0 ?
-        ul.removeChild(ul.lastElementChild) :
-        alert('Silinecek dil kalmadi');
+silBtn.onclick = function () {
+  ul.childElementCount > 0
+    ? ul.removeChild(ul.lastElementChild)
+    : alert('Silinecek dil kalmadi');
 };
 
 dilInput.addEventListener('keydown', (e) => {
-    // console.log(e);
-    if (e.keyCode === 13) {
-        ekleBtn.onclick();
-    }
-    // if (e.code === 'Enter') {
-    //   ekleBtn.onclick();
-    // }
-    if (e.code === 'Delete') {
-        silBtn.onclick();
-    }
+  // console.log(e);
+  if (e.keyCode === 13) {
+    ekleBtn.onclick();
+  }
+  // if (e.code === 'Enter') {
+  //   ekleBtn.onclick();
+  // }
+  if (e.code === 'Delete') {
+    silBtn.onclick();
+  }
 });
+
+window.onload = () => {
+  javascriptKontrol();
+  dilInput.focus();
+};

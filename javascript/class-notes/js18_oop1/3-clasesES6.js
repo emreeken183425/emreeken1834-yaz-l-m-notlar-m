40bcef2ac1236ff7aebcd117a9341adc3ee07365
emreeken183425 <emreeken1834@gmail.com>
2022-06-18 14:18:40 +0300

    loading

---
+++ javascript/class-notes/js18_oop1/3-clasesES6.js
@@ -0,0 +1,35 @@
+//* ======================================================
+//*     OOP - Inheritance and Polymorphism(ES6)
+//* ======================================================
+
+//? Bir parent class'in degisken ve fonksiyonelliği extends keyword'u ile
+//? child class'a gecmektedir.(INHERITANCE)
+
+//* Polymorphism, bir degisken, fonksiyon veya nesnenin çoklu sekiller
+//* alabilmesini tanimlayan bir nesne-yonelimli programlama teknigidir.
+//* Polymorphism, genellikle Overloading ve Overriding gibi alt kavramlar ile bilinir.
+
+class Book {
+    constructor(title, author, year) {
+      this.title = title;
+      this.author = author;
+      this.year = year;
+      // this.getTitle = function () {
+      //   return this.title;
+      // };
+    }
+  //! bu kısımda yazılan fonsiyonlar aslında prototype alanında bulunur
+    getSummary() {
+      return `${this.title} was written by ${this.author} in ${this.year} `;
+    }
+  }
+  
+  //? instance
+  const book1 = new Book('Kasagi', 'Omer Seyfettin', 1920);
+  const book2 = new Book('Sinekli Bakkal', 'H.Edip Adıvar', 1910);
+  
+  console.log(book1);
+
+  class Magazine extends Book{
+
+  }
